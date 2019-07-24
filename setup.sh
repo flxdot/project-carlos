e890e948c0feeef8838532d3f2073030c51b987b
Felix Fanghaenel <35657654+flxdot@users.noreply.github.com>
2019-07-24 23:09:45 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,61 +4,16 @@
 # Python 3.7.0 #
 ################
 
-# First install the dependencies needed to build
-sudo apt-get update
-sudo apt-get install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev
-
-# Compile (takes a while!)
-wget https://www.python.org/ftp/python/3.7.4/Python-3.7.4.tar.xz
-tar xf Python-3.7.4.tar.xz
-cd Python-3.7.4
-./configure --prefix=/usr/local/opt/python-3.7.4
-make -j 4
-
-# install
-sudo make altinstall
-
-# Make Python 3.7.4 the default version, make aliases
-sudo ln -s /usr/local/opt/python-3.7.4/bin/pydoc3.7 /usr/bin/pydoc3.7
-sudo ln -s /usr/local/opt/python-3.7.4/bin/python3.7 /usr/bin/python3.7
-sudo ln -s /usr/local/opt/python-3.7.4/bin/python3.7m /usr/bin/python3.7m
-sudo ln -s /usr/local/opt/python-3.7.4/bin/pyvenv-3.7 /usr/bin/pyvenv-3.7
-sudo ln -s /usr/local/opt/python-3.7.4/bin/pip3.7 /usr/bin/pip3.7
-alias python='/usr/bin/python3.7'
-alias python3='/usr/bin/python3.7'
-ls /usr/bin/python*
-cd ..
-sudo rm -r Python-3.7.4
-rm Python-3.7.4.tar.xz
-. ~/.bashrc
+sudo sh ./install_python.sh
 
 ############
 # InfluxDB #
 ############
 
-# install influxdb
-curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
-echo "deb https://repos.influxdata.com/debian stretch stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
-sudo apt update
-sudo apt install influxdb
-
-# edit influxdb config
-sudo nano /etc/influxdb/influxdb.conf
-
-# start influx db
-sudo systemctl enable influxdb
-sudo systemctl start influxdb
+sudo sh ./install_influxdb.sh
 
 ###########
 # grafana #
 ###########
 
-wget https://dl.grafana.com/oss/release/grafana_6.2.5_amd64.deb
-sudo dpkg -i grafana_6.2.5_amd64.deb
-
-# grafana autostart
-sudo /bin/systemctl daemon-reload
-sudo /bin/systemctl enable grafana-server
-
-# start grafana
-sudo /bin/systemctl start grafana-server
+sudo sh ./install_grafana.sh
